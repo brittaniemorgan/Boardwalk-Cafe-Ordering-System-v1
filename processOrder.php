f441ca104b142f19dc01e27971b3c6de0f847a8e
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2022-11-26 01:55:41 -0500

    Added list of pending orders

---
+++ processOrder.php
@@ -0,0 +1,17 @@
+<?php 
+    require "classes.php";
+    #orders = query to get orders from databsse
+/*    foreach($orders as $order):
+        if (order.status != "complete"):
+?>
+
+        <ul>
+            <li><?=order.toString(); ?></li>
+            <button class="markCompleted"s id= <?= $order.getOrderNum()?>>Mark as Completed</button>
+        </ul>
+        
+<?php endif;
+    endforeach;
+?>
+
+*/
