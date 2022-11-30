967d91f6b54fde4826f2398c16d00c12bb0cac57
Lenesia Brown <lenesiabrown37@gmail.com>
2022-11-30 13:44:23 -0500

    Incomplete php classes

--- customer.php
+++ customer.php
@@ -63,3 +63,62 @@
    }  
   
 ?>  
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Boardwalk Cafe</title>
+</head>
+<body>
+    <?php
+        class Customer extends Person {  
+          private $dbmanager;
+          protected $conn;
+          // var $cancel;  
+          // var $account;  
+          // var $orders;
+          // var $rewards;
+          // var $menu;
+            
+          function Customer__construct ($name, $username, $password, $dbmanager) { 
+            $this->$name = $name;
+            $this->$username = $username;
+            $this->$password = $password;
+            $this->dbmanager = $dbmanager;
+            $this->conn = $this->dbmanager->getConn(); 
+            //  $this->cancel = $cancel;
+            //  $this->account = $account;
+            //  $this->orders = $orders;
+            //  $this->rewards = $rewards;
+            //  $this->menu = $menu;
+    
+          }  
+
+
+          function viewMenu(){
+            $stmt = $this->conn->query("SELECT * FROM `menuItems`");
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+            foreach($results as $row){
+                echo " ".$row['name']." ".$row['category'];
+          }
+        }
+
+
+          function viewOrders(){
+            $stmt = $this->conn->query("SELECT * FROM `orders` WHERE `status` = 'OPEN'");
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+            foreach($results as $row){
+                echo " ".$row['items']." ".$row['total'];
+            }
+        }
+      }
+    ?>
+    
+</body>
+</html>
--- deliveryPersonnel.php
+++ deliveryPersonnel.php
@@ -1,30 +1,73 @@
-<?php  
-   class DeliveryPersonnel {  
-      var $status;  
-      var $view;  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delivery Personnel - The Boardwalk Cafe</title>
+</head>
+<body>
+    <?php
+
+      require 'Employee.php';
+      require_once 'DBManager.php';
+        class DeliveryPersonnel extends Employee {  
+            // var $status;  
+            // var $view;  
+            private $employee;
+            private $dbmanager;
+            protected $conn;
+            
+              
+            function DeliveryPersonnel__construct ($employee, $dbmanager) {  
+              $this->dbmanager = $dbmanager;
+              $this->employee = $employee;
+              $this->conn = $this->dbmanager->getConn();
+              
+              // $this->status = $status;
+              // $this->view = $view;
+            }  
+
+            function updateStatus($orderId){
+          
+              $stmt = $this->conn->prepare("UPDATE `orders` SET `status` = 'CLSD' WHERE `id` = :orderId");
+              $stmt->bindParam(':orderId', $orderId, PDO::PARAM_INT);
+              
+              if($stmt->execute()){
+                      echo 'Status Updated';
+              }else{
+                      echo 'Could Not Update Status';
+              }      
+          }
+
+
+          function viewOrdersMinor () {
+              $stmt = $this->conn->query("SELECT * FROM `orders` WHERE `status` = 'OPEN'");
+              $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+              foreach($results as $row){
+                  echo " ".$row['items']." ".$row['address']." ".$row['total'];
+              }
+          }
+
+
+            
+          //   function get_status(){
+          //     return $this->status;
+          //   }
+
+          //   function get_view(){
+          //     return $this->view;
+          //   }
+
+
+          //   function updateStatus (){
+
+          // } 
+            
+        }  
         
-      function DeliveryPersonnel__construct ($status, $view) {  
-         $this->status = $status;
-         $this->view - $view;
-      }  
-      
-      function get_status(){
-        return $this->status;
-      }
-
-      function get_view(){
-        return $this->view;
-      }
-
-
-      function updateStatus (){
-
-    }
-
-
-      function viewOrdersMinor (){
-
-    }
-   }  
-  
-?>
+    ?>
+    
+</body>
+</html>
