a6f5e10fbc60c989e58ff90b8622c981c808bfae
Lenesia Brown <lenesiabrown37@gmail.com>
2022-12-02 09:30:29 -0500

    Added some css

--- login.php
+++ login.php
@@ -26,15 +26,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
     <title>Log In</title>
 </head>
 <body>
+    <div class="logMain">
+    <h3>Login</h3>
     <form action="login.php" method="post">
         <label for="username">Username</label>
-        <input type="text" name="username" placeholder="username" required>
+        <input type="text" name="username" placeholder="username" required class="username">
+        <br><br>
         <label for="password">Password</label>
-        <input type="password" name="password" placeholder="password" required>
-        <input type="submit" name="submit" value="Login" />
+        <input type="password" name="password" placeholder="password" required class="password">
+        <br><br>
+        <input type="submit" name="submit" value="Login" class="login"/>
     </form>
     <?php if ($error_message != "") { ?>
     <div class="error">
@@ -42,6 +47,7 @@
     </div>
     <?php } ?>
     <a href="userSignUp.php">Create New Account</a>
+    </div>
     
 </body>
 </html>
